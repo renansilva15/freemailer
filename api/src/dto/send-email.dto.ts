97e81117f43f0df8f81e5a3a67377d661f0c20b2
renansilva15 <renangabrielsilva150@gmail.com>
2024-05-20 15:50:23 -0300

    docs(api): add swagger documentation

--- api/src/dto/send-email.dto.ts
+++ api/src/dto/send-email.dto.ts
@@ -1,24 +1,48 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class SendEmailDto {
+  @ApiProperty({
+    description: 'Email or list of emails to send',
+    example: 'example@example.com',
+  })
   @IsString({ each: true })
   @IsNotEmpty()
   to: string | string[];
 
+  @ApiProperty({
+    description: 'Subject of the email',
+    example: 'Meeting Reminder',
+  })
   @IsString()
   @IsNotEmpty()
   subject: string;
 
+  @ApiProperty({
+    description: 'Text content of the email',
+    example: 'This is the plain text content of the email.',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   text?: string;
 
+  @ApiProperty({
+    description: 'HTML content of the email',
+    example: '<p>This is the <strong>HTML</strong> content of the email.</p>',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   html?: string;
 
+  @ApiProperty({
+    description: "Sender's Name",
+    example: 'John Doe',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
