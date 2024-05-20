6dd10f695d7e0ae2d394a73e2234d30a5bf892d5
renansilva15 <renangabrielsilva150@gmail.com>
2024-05-19 22:13:13 -0300

    feat(ms-email): add email queue consumer

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
     ClientsModule.register([
       {
         name: 'QUEUE_SERVICE',
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://user:password@127.0.0.1:5672'],
+          urls: [process.env.RMQ_URL],
           queue: 'emails_queue',
           queueOptions: {
             durable: false,
--- ms-email/src/main.ts
+++ ms-email/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RMQ_URL],
+        queue: 'emails_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
