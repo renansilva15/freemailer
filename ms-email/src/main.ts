2d0bd53181f398951cf29abf42084860d378645d
Renan Gabriel Gonçalves Silva <renangabrielsilva150@gmail.com>
2024-05-22 19:25:06 +0000

    build(`main.ts`): add dummy server to expose render port

--- ms-email/src/main.ts
+++ ms-email/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as express from 'express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -18,5 +20,19 @@ async function bootstrap() {
   );
 
   await app.listen();
+
+  const dummyServer = express()
+
+  dummyServer.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  const configService = app.get(ConfigService)
+
+  const port = Number(configService.get('PORT', 3334))
+
+  dummyServer.listen(port, () => {
+    console.log(`Server listen on port ${port}`);
+  })
 }
 bootstrap();
